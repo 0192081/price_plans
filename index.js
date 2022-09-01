4df694c7e89ffa0ab50239910b74e6a16f5cf430
0192081 <thoko.sibeko2@gmail.com>
2022-09-01 10:14:03 +0200

    added port config for heroku

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ const app = express();
 //make req.body work
 app.use(express.json());
 
-const  db = await  sqlite.open({
-    filename:  './data_plan.db',
-    driver:  sqlite3.Database
-}); 
+const db = await sqlite.open({
+    filename: './data_plan.db',
+    driver: sqlite3.Database
+});
 
 console.log('db initialized');
 
 await db.migrate();
 
-app.post(`/api/phonebill/`, async function (req, res){
-     const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price
+app.get('/', function (req, res) {
+    res.json({
+        server_state: `Serving API on port: ${process.env.PORT}`
+    })
+})
+
+app.post(`/api/phonebill/`, async function (req, res) {
+    const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price
     FROM price_plan where plan_name = ?`, req.body.price_plan);
 
-     const activity = req.body.actions;
+    const activity = req.body.actions;
 
     //totalPhoneBill logic 
     const activities = activity.split(",");
@@ -37,18 +43,18 @@ app.post(`/api/phonebill/`, async function (req, res){
     res.json({
         total
     })
-  });
+});
 
-app.get(`/api/price_plans`, async function(req , res){
+app.get(`/api/price_plans`, async function (req, res) {
     const price_plans = await db.all(`select * from price_plan`)
-    res.json ({
-        price_plans 
+    res.json({
+        price_plans
     })
 });
 
 console.log("done!");
 
-const PORT = 6001;
-app.listen(PORT , function(){
+const PORT = process.env.PORT || 6001;
+app.listen(PORT, function () {
     console.log(`Price plan API started on port ${PORT}`)
 });
